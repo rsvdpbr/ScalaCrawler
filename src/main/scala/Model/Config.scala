26efe2c8d9db6d4b63da1c7a306448a607c84bf5
Ryo Nishikawa <ryo.nishikawa.re@gmail.com>
2012-11-08 19:01:57 +0900

    ConfigModelをappModelに対応 設定セット（set_nameフィールド）という概念を追加

--- src/main/scala/Model/Config.scala
+++ src/main/scala/Model/Config.scala
@@ -1,13 +1,42 @@
-package app.crawler.Table
+package app.crawler.Model
 
 import org.scalaquery.ql._
 import org.scalaquery.ql.basic.BasicDriver.Implicit._
+import org.scalaquery.simple.StaticQuery._
+import org.scalaquery.session.Database.threadLocalSession
 import basic.{ BasicTable => Table }
 
-object Config extends Table[(String, String, Option[String])]("configs") {
-  def key = column[String]("key", O PrimaryKey)
+object Config extends Table[(Int, String, String, String, Option[String])]("configs") {
+
+  // define fields
+  def id = column[Int]("id", O PrimaryKey)
+  def set_name = column[String]("set_name", O DBType "varchar(255)")
+  def key = column[String]("key", O NotNull)
   def value = column[String]("value", O DBType "varchar(255)")
   def modified = column[Option[String]]("modified")
-  def * = key ~ value ~ modified
+  def * = id ~ set_name ~ key ~ value ~ modified
+  // define variables
+  var useSet: String = null
+
+  // changeSnetting
+  def changeSetting(name: String): Unit = { useSet = name }
+  // get where query regarding set_name
+  def getSettingQuery(): String = {
+    return useSet match {
+      case null => "1 = 1"
+      case _: String => "`set_name` = '" + useSet + "'"
+    }
+  }
+
+  // get config value
+  def getByKey(key: String): String = {
+    val result = AppModel.execute.withSession {
+      queryNA[(String)](
+		"""SELECT `value` FROM `configs` WHERE `key` = '""" + key + """' AND """ + getSettingQuery()
+	  ).list
+    }
+    result(0)
+  }
+
 }
 
